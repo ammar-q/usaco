d4aae3e99b7f61c9fa4e1e86bc86203ee5ce1335
Ammar Qadri <ammar.q0@gmail.com>
2015-06-12 16:07:55 -0400

    Improved Makefile and test.c

--- Ch3/test.c
+++ Ch3/test.c
@@ -1,26 +1,14 @@
 /*
 ID: ammar.q2
 LANG: C
-TASK: ride
+TASK: $TARGET
 */
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int f(char *line) {
-	long int m = 1; 
-	char *cur;
-	for (cur = line; *cur != '\0'; cur ++) 
-		m *= *cur - 'A' + 1;
-	return m % 47;
-}
-
-main () {
-    FILE *fin  = fopen ("ride.in", "r");
-    FILE *fout = fopen ("ride.out", "w");
-    char line[1000];
-    fscanf (fin, "%s", line);	
-    int a = f(line);
-    fscanf (fin, "%s", line);
-    int b = f(line);
-    fprintf (fout, "%s\n", a == b? "GO" : "STAY");
-    return 0;
+int main () {
+  FILE *fin  = fopen ("$TARGET.in", "r");
+  FILE *fout = fopen ("$TARGET.out", "w");
+  return 0;
 }
